82769f77462636d7bd52cdcf388778c40717eb9c
Chi Bong Ho <cbho@pih.org>
2024-11-21 14:11:54 -0500

    (feat) UHM-8241 Create O2 pregnancy / infant dashboard extension for the ward app

--- packages/esm-commons-app/src/ward-app/types.ts
+++ packages/esm-commons-app/src/ward-app/types.ts
@@ -1,11 +1,28 @@
 import type { DefaultWorkspaceProps, Patient, Visit } from '@openmrs/esm-framework';
 
-// a stripped down version of the same type defined in esm-ward-app
+// stripped down versions of the same types defined in esm-ward-app
+
 export type WardPatient = {
   patient: Patient;
   visit: Visit;
+  // other fields not typed
 };
 
 export interface WardPatientWorkspaceProps extends DefaultWorkspaceProps {
   wardPatient: WardPatient;
+}
+
+export interface PatientAndAdmission {
+  patient: Patient;
+  // other fields not typed
+}
+
+export interface MotherChildRelationships {
+  motherByChildUuid: Map<string, PatientAndAdmission>;
+  childrenByMotherUuid: Map<string, PatientAndAdmission[]>;
+  isLoading: boolean;
+}
+
+export interface MaternalWardViewContext {
+  motherChildRelationships: MotherChildRelationships;
 }
