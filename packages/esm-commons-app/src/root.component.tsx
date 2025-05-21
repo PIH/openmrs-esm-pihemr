d17f66ebf15fc5202090ef1b58833991fb48a463
Chi Bong Ho <cbho@pih.org>
2025-05-21 09:28:34 -0400

    UHM-8242: add root component to mount ward / appointments / queues app

---
+++ packages/esm-commons-app/src/root.component.tsx
@@ -0,0 +1,45 @@
+import { ExtensionSlot, WorkspaceContainer } from '@openmrs/esm-framework';
+import React from 'react';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+
+export default function Root() {
+  return (
+    <div>
+      <BrowserRouter basename={window.spaBase}>
+        <Routes>
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/service-queues/queue-table-by-status/:customQueueUuid" element={<QueueTableByStatus />} />
+          <Route path="/ward" element={<Ward />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Appointments() {
+  return (
+    <>
+      <WorkspaceContainer overlay contextKey="appointments" />
+      <ExtensionSlot name={'appointments-dashboard-slot'} />
+    </>
+  );
+}
+
+function QueueTableByStatus() {
+  const { customQueueUuid } = useParams();
+  return (
+    <>
+      <WorkspaceContainer overlay contextKey="service-queues" />
+      <ExtensionSlot name={'queue-table-by-status-view-slot'} state={{ customQueueUuid }} />
+    </>
+  );
+}
+
+function Ward() {
+  return (
+    <>
+      <WorkspaceContainer overlay contextKey="ward" />
+      <ExtensionSlot name={'ward-view-slot'} />
+    </>
+  );
+}
