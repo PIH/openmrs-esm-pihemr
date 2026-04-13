682b5ffedae16d5e8daf24f8d78f15f71aa7ea57
chibongho <cbho@pih.org>
2026-04-13 09:51:28 -0400

    UHM-9178 add E2E tests for service queues app (#30)
    * UHM-9178 add E2E tests for service queues app
    
    * fixup

--- e2e/commands/visit-operations.ts
+++ e2e/commands/visit-operations.ts
@@ -26,7 +26,6 @@ export const endVisit = async (api: APIRequestContext, uuid: string, isWardTest 
   await api.post(`visit/${uuid}`, {
     data: {
       location: isWardTest ? process.env.E2E_WARD_LOCATION_UUID : process.env.E2E_LOGIN_DEFAULT_LOCATION_UUID,
-      startDatetime: dayjs().subtract(1, 'D').format('YYYY-MM-DDTHH:mm:ss.SSSZZ'),
       visitType: KGHVisitType,
       stopDatetime: dayjs().format('YYYY-MM-DDTHH:mm:ss.SSSZZ'),
     },
--- e2e/pages/index.ts
+++ e2e/pages/index.ts
@@ -1,1 +1,2 @@
 export * from './ward-page';
+export * from './service-queues-page';
