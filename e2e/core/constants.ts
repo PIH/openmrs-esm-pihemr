f23aa6cc57550fbf1b5b806159aceaa79fb7ec9f
chibongho <cbho@pih.org>
2026-04-17 09:53:42 -0400

    UHM-9180 E2E Dispensing tests (#33)

--- e2e/core/constants.ts
+++ e2e/core/constants.ts
@@ -3,6 +3,7 @@ export const KGHLocationsUuids = {
   'Labour and Delivery': '11377a5b-6850-11ee-ab8d-0242ac120002',
   'Antenatal Ward': '11f5c9f9-40b8-46ad-9e7e-59473ce43246',
   KGH: '074b2ab0-716a-11eb-8aa6-0242ac110002',
+  'Maternity Pharmacy': '84b9b680-786c-4388-9e7c-805614c13b5a',
 };
 
 export const KGHEmrIdSourceUuid = '809b23e3-7162-11eb-8aa6-0242ac110002';
